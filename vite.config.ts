67b2da6b9f31ee6bb252c22c9260d6845ebb0e17
tuaa62-byte <tuaa62@gmail.com>
2026-09-18 18:54:30 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import {defineConfig, Plugin} from 'vite';
 // LINT.IfChange(aistudio_media_plugin)
 function aistudioMediaPlugin(): Plugin {
   return {
-    Base:'/TKBHIEN/',
+    Base:'/lbgHIEN/',
     name: 'vite-plugin-aistudio-media',
     configureServer(server) {
       server.middlewares.use((req, res, next) => {
